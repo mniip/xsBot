3bcb86858e8c676c24092633344fac0f57b755fb
mniip <mniip@mniip.com>
2013-04-02 05:35:36 +0400

    better hello plugin

--- plugins/hello.lua
+++ plugins/hello.lua
@@ -1,13 +1,19 @@
 local function check(network,sender,_,recipient,text)
 	text=text:lower()
-	local word1,word2=text:match"^%W*(%w+)[, ]+(%w+)%W*$"
-	if word1 then
-		word1,word2=word1:lower(),word2:lower()
-		if word1 then
-			if (word1=="hi" or word1=="hello" or word1=="ohai")and(word2=="all" or word2=="there" or word2==servers[network].nick:lower()) then
-				privmsg(network,recipient,word1..", "..splitnick(sender))
-			end
-		end
+	local words={}
+	for word in text:gmatch"%w+" do
+		table.insert(words,word:lower())
+	end
+	if words[1]==servers[network].nick:lower() then
+		table.remove(words,1)
+	end
+	if words[#words]==servers[network].nick:lower() then
+		table.remove(words)
+	end
+	local hsentences={hi_all=true,bye_all=false,ohai_all=true,hello_there=true,hi_there=true,ohai_there=true,hello_there=true,i_gtg=false,gotta_go=false,gtg=false,got_to_go=false,ohaider=true,goodbye_all=false,hi=true,hello=true}
+	local sentence=table.concat(words,"_")
+	if hsentences[sentence]~=nil then
+		privmsg(network,recipient,(hsentences[sentence]and"Hi"or"Bye")..", "..splitnick(sender))
 	end
 end
 local function load()
